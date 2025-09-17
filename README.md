98de082054ce832df04002c7ed29a53cf02f1e2e
Igor Mosyagin <408698+cra@users.noreply.github.com>
2025-09-17 09:23:46 +0200

    update hw1 to match 2025 hw

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # ML ZoomCamp 2025cohort
 
-## hw1
+## hw1: [intro](https://github.com/DataTalksClub/machine-learning-zoomcamp/blob/master/cohorts/2025/01-intro/homework.md)
 
-```
+[my solution](./hw1)
+
+```bash
 cd hw1
 make help
 uv sync
-make get-data
 make q1
+make q2
+...
 ```
--- hw1/q7.py
+++ hw1/q7.py
@@ -1,14 +1,25 @@
-import pandas as pd
+import sys
+
 import numpy as np
+import pandas as pd
 
-df = pd.read_csv('laptops.csv')
-masq = df['Brand'] == 'Innjoo'
-features = ['RAM', 'Storage', 'Screen']
-prices = [1100, 1300, 800, 900, 1000, 1100]
 
-X = df[masq][features].to_numpy()
-XTX = X.T @ X
-XTX_inv = np.linalg.inv(XTX)
-y = np.array(prices)
-w = XTX_inv @ X.T @ y
-print(sum(w))
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python q7.py <csv_file>")
+        sys.exit(1)
+
+    csv_file = sys.argv[1]
+    df = pd.read_csv(csv_file)
+    masq = df["origin"] == "Asia"
+    features = ["vehicle_weight", "model_year"]
+    y = [1100, 1300, 800, 900, 1000, 1100, 1200]
+
+    X = df[masq][features][:7].to_numpy()
+    XTX_inv = np.linalg.inv(X.T @ X)
+    w = XTX_inv @ X.T @ y
+    print(sum(w))
+
+
+if __name__ == "__main__":
+    main()
